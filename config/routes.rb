affe1aa528d8965b97d9ecec339fb4185b313274
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-07 15:54:49 -0700

    setting up /vendors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/", to: "home#index"
+  get "/vendors", to: "vendors#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
